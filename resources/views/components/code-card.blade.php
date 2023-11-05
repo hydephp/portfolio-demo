3a27bf4267a67a68fdc99bf644304eaa71ae02d0
Caen De Silva <caen@desilva.se>
2023-11-05 15:43:41 +0100

    Switch to table-based layout
    Allows us to wrap text while keeping the left padding

--- resources/views/components/code-card.blade.php
+++ resources/views/components/code-card.blade.php
@@ -10,37 +10,134 @@
 
     <article>
         <main>
+            <style>
+                .line-number {
+                    color:#3A3F58;
+                    text-align: right;
+                    -webkit-user-select: none;
+                    user-select: none;
+                    padding-left: 1rem;
+                }
+
+                table {
+                    border-collapse: collapse;
+                    border-spacing: 0;
+                    border: 0;
+                }
+
+                td {
+                    padding: 0;
+                }
+            </style>
             <pre style="white-space: nowrap">
                 <code data-theme="material-theme-palenight" data-lang="markdown" class="torchlight" style="background-color: #292D3E; --theme-selection-background: #00000080;">
-                    <!-- Syntax highlighted by torchlight.dev -->
-                    <div class="line"><span style="color:#3A3F58; text-align: right; -webkit-user-select: none; user-select: none;" class="line-number">&nbsp;1</span><span style="color: #89DDFF;">## </span><span style="color: #FFCB6B;">Hey there, I'm Jane Doe!</span></div>
-                    <div class="line"><span style="color:#3A3F58; text-align: right; -webkit-user-select: none; user-select: none;" class="line-number">&nbsp;2</span>&nbsp;</div>
-                    <div class="line"><span style="color:#3A3F58; text-align: right; -webkit-user-select: none; user-select: none;" class="line-number">&nbsp;3</span><span style="color: #89DDFF;"># </span><span style="color: #FFCB6B;">Freelance photographer</span></div>
-                    <div class="line"><span style="color:#3A3F58; text-align: right; -webkit-user-select: none; user-select: none;" class="line-number">&nbsp;4</span>&nbsp;</div>
-                    <div class="line"><span style="color:#3A3F58; text-align: right; -webkit-user-select: none; user-select: none;" class="line-number">&nbsp;5</span><span style="color: #89DDFF;">![</span><span style="color: #C3E88D;">Jane Doe</span><span style="color: #89DDFF;">]</span><span style="color: #89DDFF;">(</span><span style="color: #A6ACCD;">media/photographer.jpg</span><span style="color: #89DDFF;">)</span></div>
-                    <div class="line"><span style="color:#3A3F58; text-align: right; -webkit-user-select: none; user-select: none;" class="line-number">&nbsp;6</span>&nbsp;</div>
-                    <div class="line"><span style="color:#3A3F58; text-align: right; -webkit-user-select: none; user-select: none;" class="line-number">&nbsp;7</span><span style="color: #89DDFF;">## </span><span style="color: #FFCB6B;">About</span></div>
-                    <div class="line"><span style="color:#3A3F58; text-align: right; -webkit-user-select: none; user-select: none;" class="line-number">&nbsp;8</span>&nbsp;</div>
-                    <div class="line"><span style="color:#3A3F58; text-align: right; -webkit-user-select: none; user-select: none;" class="line-number">&nbsp;9</span><span style="color: #A6ACCD;">This is a demo website built with </span><span style="color: #89DDFF;">[</span><span style="color: #C3E88D;">HydePHP</span><span style="color: #89DDFF;">]</span><span style="color: #89DDFF;">(</span><span style="color: #F07178;">http://hydephp.com/</span><span style="color: #89DDFF;">)</span><span style="color: #A6ACCD;">, a free and open source static site generator. This entire site is built using just Markdown text that is transformed into a fast and secure website that can be published anywhere.</span></div>
-                    <div class="line"><span style="color:#3A3F58; text-align: right; -webkit-user-select: none; user-select: none;" class="line-number">10</span>&nbsp;</div>
-                    <div class="line"><span style="color:#3A3F58; text-align: right; -webkit-user-select: none; user-select: none;" class="line-number">11</span><span style="color: #A6ACCD;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget consectetur consectetur, nisi nisl aliquet nisi, euismod consectetur nisi nisi euismod nisi.</span></div>
-                    <div class="line"><span style="color:#3A3F58; text-align: right; -webkit-user-select: none; user-select: none;" class="line-number">12</span>&nbsp;</div>
-                    <div class="line"><span style="color:#3A3F58; text-align: right; -webkit-user-select: none; user-select: none;" class="line-number">13</span><span style="color: #89DDFF;">## </span><span style="color: #FFCB6B;">Gallery</span></div>
-                    <div class="line"><span style="color:#3A3F58; text-align: right; -webkit-user-select: none; user-select: none;" class="line-number">14</span>&nbsp;</div>
-                    <div class="line"><span style="color:#3A3F58; text-align: right; -webkit-user-select: none; user-select: none;" class="line-number">15</span><span style="color: #89DDFF;">![</span><span style="color: #C3E88D;">Austria</span><span style="color: #89DDFF;">]</span><span style="color: #89DDFF;">(</span><span style="color: #A6ACCD;">media/austria.jpg</span><span style="color: #89DDFF;">)</span></div>
-                    <div class="line"><span style="color:#3A3F58; text-align: right; -webkit-user-select: none; user-select: none;" class="line-number">16</span><span style="color: #89DDFF;">![</span><span style="color: #C3E88D;">Boat</span><span style="color: #89DDFF;">]</span><span style="color: #89DDFF;">(</span><span style="color: #A6ACCD;">media/boat.jpg</span><span style="color: #89DDFF;">)</span></div>
-                    <div class="line"><span style="color:#3A3F58; text-align: right; -webkit-user-select: none; user-select: none;" class="line-number">17</span><span style="color: #89DDFF;">![</span><span style="color: #C3E88D;">Croatia</span><span style="color: #89DDFF;">]</span><span style="color: #89DDFF;">(</span><span style="color: #A6ACCD;">media/croatia.jpg</span><span style="color: #89DDFF;">)</span></div>
-                    <div class="line"><span style="color:#3A3F58; text-align: right; -webkit-user-select: none; user-select: none;" class="line-number">18</span><span style="color: #89DDFF;">![</span><span style="color: #C3E88D;">Fireworks</span><span style="color: #89DDFF;">]</span><span style="color: #89DDFF;">(</span><span style="color: #A6ACCD;">media/fireworks.jpg</span><span style="color: #89DDFF;">)</span></div>
-                    <div class="line"><span style="color:#3A3F58; text-align: right; -webkit-user-select: none; user-select: none;" class="line-number">19</span><span style="color: #89DDFF;">![</span><span style="color: #C3E88D;">Hallstatt</span><span style="color: #89DDFF;">]</span><span style="color: #89DDFF;">(</span><span style="color: #A6ACCD;">media/hallstatt.jpg</span><span style="color: #89DDFF;">)</span></div>
-                    <div class="line"><span style="color:#3A3F58; text-align: right; -webkit-user-select: none; user-select: none;" class="line-number">20</span><span style="color: #89DDFF;">![</span><span style="color: #C3E88D;">Lemonade</span><span style="color: #89DDFF;">]</span><span style="color: #89DDFF;">(</span><span style="color: #A6ACCD;">media/lemonade.jpg</span><span style="color: #89DDFF;">)</span></div>
-                    <div class="line"><span style="color:#3A3F58; text-align: right; -webkit-user-select: none; user-select: none;" class="line-number">21</span><span style="color: #A6ACCD;">{.gallery}</span></div>
-                    <div class="line"><span style="color:#3A3F58; text-align: right; -webkit-user-select: none; user-select: none;" class="line-number">22</span>&nbsp;</div>
-                    <div class="line"><span style="color:#3A3F58; text-align: right; -webkit-user-select: none; user-select: none;" class="line-number">23</span>&nbsp;</div>
-                    <div class="line"><span style="color:#3A3F58; text-align: right; -webkit-user-select: none; user-select: none;" class="line-number">24</span><span style="color: #89DDFF;">## </span><span style="color: #FFCB6B;">Contact</span></div>
-                    <div class="line"><span style="color:#3A3F58; text-align: right; -webkit-user-select: none; user-select: none;" class="line-number">25</span>&nbsp;</div>
-                    <div class="line"><span style="color:#3A3F58; text-align: right; -webkit-user-select: none; user-select: none;" class="line-number">26</span><span style="color: #89DDFF;">-</span><span style="color: #A6ACCD;"> Phone: +1 (123) 456-7890</span></div>
-                    <div class="line"><span style="color:#3A3F58; text-align: right; -webkit-user-select: none; user-select: none;" class="line-number">27</span><span style="color: #89DDFF;">-</span><span style="color: #A6ACCD;"> Email: jane.doe@example.com</span></div>
-                    <div class="line"><span style="color:#3A3F58; text-align: right; -webkit-user-select: none; user-select: none;" class="line-number">28</span><span style="color: #89DDFF;">-</span><span style="color: #A6ACCD;"> Address: 123 Main St, Anytown, CA 12345</span></div>
+                    <table>
+                        <tbody>
+                        <!-- Syntax highlighted by torchlight.dev -->
+                        <tr class="line">
+                            <td><span class="line-number">&nbsp;1</span></td>
+                            <td><span style="color: #89DDFF;">## </span><span style="color: #FFCB6B;">Hey there, I'm Jane Doe!</span></td>
+                        </tr>
+                        <tr class="line">
+                            <td><span class="line-number">&nbsp;2</span></td>
+                            <td>&nbsp;</tr>
+                        <tr class="line">
+                            <td><span class="line-number">&nbsp;3</span></td>
+                            <td><span style="color: #89DDFF;"># </span><span style="color: #FFCB6B;">Freelance photographer</span></td>
+                        </tr>
+                        <tr class="line">
+                            <td><span class="line-number">&nbsp;4</span></td>
+                            <td>&nbsp;</tr>
+                        <tr class="line">
+                            <td><span class="line-number">&nbsp;5</span></td>
+                            <td><span style="color: #89DDFF;">![</span><span style="color: #C3E88D;">Jane Doe</span><span style="color: #89DDFF;">]</span><span style="color: #89DDFF;">(</span><span style="color: #A6ACCD;">media/photographer.jpg</span><span style="color: #89DDFF;">)</span></td>
+                        </tr>
+                        <tr class="line">
+                            <td><span class="line-number">&nbsp;6</span></td>
+                            <td>&nbsp;</tr>
+                        <tr class="line">
+                            <td><span class="line-number">&nbsp;7</span></td>
+                            <td><span style="color: #89DDFF;">## </span><span style="color: #FFCB6B;">About</span></td>
+                        </tr>
+                        <tr class="line">
+                            <td><span class="line-number">&nbsp;8</span></td>
+                            <td>&nbsp;</tr>
+                        <tr class="line">
+                            <td><span class="line-number">&nbsp;9</span></td>
+                            <td><span style="color: #A6ACCD;">This is a demo website built with </span><span style="color: #89DDFF;">[</span><span style="color: #C3E88D;">HydePHP</span><span style="color: #89DDFF;">]</span><span style="color: #89DDFF;">(</span><span style="color: #F07178;">http://hydephp.com/</span><span style="color: #89DDFF;">)</span><span style="color: #A6ACCD;">, a free and open source static site generator. This entire site is built using just Markdown text that is transformed into a fast and secure website that can be published anywhere.</span></td>
+                        </tr>
+                        <tr class="line">
+                            <td><span class="line-number">10</span></td>
+                            <td>&nbsp;</tr>
+                        <tr class="line">
+                            <td><span class="line-number">11</span></td>
+                            <td><span style="color: #A6ACCD;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget consectetur consectetur, nisi nisl aliquet nisi, euismod consectetur nisi nisi euismod nisi.</span></td>
+                        </tr>
+                        <tr class="line">
+                            <td><span class="line-number">12</span></td>
+                            <td>&nbsp;</tr>
+                        <tr class="line">
+                            <td><span class="line-number">13</span></td>
+                            <td><span style="color: #89DDFF;">## </span><span style="color: #FFCB6B;">Gallery</span></td>
+                        </tr>
+                        <tr class="line">
+                            <td><span class="line-number">14</span></td>
+                            <td>&nbsp;</tr>
+                        <tr class="line">
+                            <td><span class="line-number">15</span></td>
+                            <td><span style="color: #89DDFF;">![</span><span style="color: #C3E88D;">Austria</span><span style="color: #89DDFF;">]</span><span style="color: #89DDFF;">(</span><span style="color: #A6ACCD;">media/austria.jpg</span><span style="color: #89DDFF;">)</span></td>
+                        </tr>
+                        <tr class="line">
+                            <td><span class="line-number">16</span></td>
+                            <td><span style="color: #89DDFF;">![</span><span style="color: #C3E88D;">Boat</span><span style="color: #89DDFF;">]</span><span style="color: #89DDFF;">(</span><span style="color: #A6ACCD;">media/boat.jpg</span><span style="color: #89DDFF;">)</span></td>
+                        </tr>
+                        <tr class="line">
+                            <td><span class="line-number">17</span></td>
+                            <td><span style="color: #89DDFF;">![</span><span style="color: #C3E88D;">Croatia</span><span style="color: #89DDFF;">]</span><span style="color: #89DDFF;">(</span><span style="color: #A6ACCD;">media/croatia.jpg</span><span style="color: #89DDFF;">)</span></td>
+                        </tr>
+                        <tr class="line">
+                            <td><span class="line-number">18</span></td>
+                            <td><span style="color: #89DDFF;">![</span><span style="color: #C3E88D;">Fireworks</span><span style="color: #89DDFF;">]</span><span style="color: #89DDFF;">(</span><span style="color: #A6ACCD;">media/fireworks.jpg</span><span style="color: #89DDFF;">)</span></td>
+                        </tr>
+                        <tr class="line">
+                            <td><span class="line-number">19</span></td>
+                            <td><span style="color: #89DDFF;">![</span><span style="color: #C3E88D;">Hallstatt</span><span style="color: #89DDFF;">]</span><span style="color: #89DDFF;">(</span><span style="color: #A6ACCD;">media/hallstatt.jpg</span><span style="color: #89DDFF;">)</span></td>
+                        </tr>
+                        <tr class="line">
+                            <td><span class="line-number">20</span></td>
+                            <td><span style="color: #89DDFF;">![</span><span style="color: #C3E88D;">Lemonade</span><span style="color: #89DDFF;">]</span><span style="color: #89DDFF;">(</span><span style="color: #A6ACCD;">media/lemonade.jpg</span><span style="color: #89DDFF;">)</span></td>
+                        </tr>
+                        <tr class="line">
+                            <td><span class="line-number">21</span></td>
+                            <td><span style="color: #A6ACCD;">{.gallery}</span></td>
+                        </tr>
+                        <tr class="line">
+                            <td><span class="line-number">22</span></td>
+                            <td>&nbsp;</tr>
+                        <tr class="line">
+                            <td><span class="line-number">23</span></td>
+                            <td>&nbsp;</tr>
+                        <tr class="line">
+                            <td><span class="line-number">24</span></td>
+                            <td><span style="color: #89DDFF;">## </span><span style="color: #FFCB6B;">Contact</span></td>
+                        </tr>
+                        <tr class="line">
+                            <td><span class="line-number">25</span></td>
+                            <td>&nbsp;</tr>
+                        <tr class="line">
+                            <td><span class="line-number">26</span></td>
+                            <td><span style="color: #89DDFF;">-</span><span style="color: #A6ACCD;"> Phone: +1 (123) 456-7890</span></td>
+                        </tr>
+                        <tr class="line">
+                            <td><span class="line-number">27</span></td>
+                            <td><span style="color: #89DDFF;">-</span><span style="color: #A6ACCD;"> Email: jane.doe@example.com</span></td>
+                        </tr>
+                        <tr class="line">
+                            <td><span class="line-number">28</span></td>
+                            <td><span style="color: #89DDFF;">-</span><span style="color: #A6ACCD;"> Address: 123 Main St, Anytown, CA 12345</span></td>
+                        </tr>
+                        </tbody>
+                    </table>
                 </code>
             </pre>
         </main>
